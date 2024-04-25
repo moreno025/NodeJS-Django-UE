41e707f89604aa801df9bba6496cee410fe0ba63
moreno025 <22342474@live.uem.es>
2024-04-25 18:46:27 +0200

    renombre carpetas

--- ejercicios_modulos/ejercicio2.js
+++
@@ -1,9 +0,0 @@
-const {readFile} = require('fs').promises
-
-async function printFile() {
-    const file = await readFile('./ejemplo.txt', 'utf-8')
-    console.log(file)
-}
-
-printFile()
-console.log('Haz esto primero')
